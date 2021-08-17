52bb8da214fa16db6b6828d0dca10c61b22cb997
Shyamal Madura <shyamalmadura@users.noreply.github.com>
2021-08-17 22:09:23 +0530

    Updated to a minimalistic web server

--- src/main/java/com/github/shyamalmadura/HelloServerVertx.java
+++ src/main/java/com/github/shyamalmadura/HelloServerVertx.java
@@ -1,22 +1,12 @@
 package com.github.shyamalmadura;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Future;
+import io.vertx.core.Vertx;
 
-public class HelloServerVertx extends AbstractVerticle {
-
-    @Override
-    public void start(Future<Void> future) {
-        vertx.createHttpServer()
-                .requestHandler(
-                  r -> r.response().end("Welcome to Vert.x Intro"))
-                .listen(config().getInteger("http.port", 8080), result -> {
-                    if (result.succeeded()) {
-                        future.complete();
-                    } else {
-                        future.fail(result.cause());
-                    }
-                });
-    }
+public class HelloServerVertx {
+    
+    public static void main(String[] args) {
+        // Create an HTTP server which simply returns "Hello World!" to each request.
+        Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+      }
 
 }
